c99ab10bd0b0ba846bfbd69554cb7bdbad96b71e
k3b <klaus3b-github@yahoo.de>
2015-01-19 08:32:03 +0100

    Added gpl

--- LocationMapViewer/src/main/java/de/k3b/android/locationMapViewer/geo/OsmGpxLocationReader.java
+++ LocationMapViewer/src/main/java/de/k3b/android/locationMapViewer/geo/OsmGpxLocationReader.java
@@ -5,7 +5,7 @@ import de.k3b.geo.io.gpx.GpxReader;
 /**
  * Created by EVE on 07.01.2015.
  */
-public class OsmGpxLocationReader  extends GpxReader<OsmPoiGeoPointDto> {
+public class OsmGpxLocationReader extends GpxReader<OsmPoiGeoPointDto> {
     @Override
     public OsmPoiGeoPointDto getNewInstance() {
         return new OsmPoiGeoPointDto();
